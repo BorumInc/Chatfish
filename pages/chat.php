7b707a9b07d4cb1f8b760a77b3427a3e972de1ae
Varun <31084854+Borumer@users.noreply.github.com>
2020-04-06 11:16:48 -0400

    Start Chatfish website with styles, meta, and icon

---
+++ pages/chat.php
@@ -0,0 +1,17 @@
+<?php
+	$page_title = "Chat - Chatfish";
+	include('header.html');
+?>
+</head>
+<body>
+	<div>
+		<input type = "text">
+		<button>Send</button>
+	</div>
+	<?php
+
+
+
+	?>
+</body>
+</html>
