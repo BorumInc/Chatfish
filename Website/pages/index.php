5b5e61a3edbc699e2223d9cc3b896c11e308d7e8
Varun <31084854+Borumer@users.noreply.github.com>
2020-07-14 12:25:44 -0400

    Show download link on website

---
+++ Website/pages/index.php
@@ -0,0 +1,22 @@
+<?php
+$page_title = "Chatfish | Swim Chatfish, swim";
+include('header.html');
+?>
+<link href = "css/home.css" rel = "stylesheet" type = "text/css">
+</head>
+<body>
+<h1>Communication reimagined</h1>
+<a id = "chat-button" href = "">Start chatting</a>
+<div class = "windows">
+    <h2>Windows</h2>
+    <ol>    
+        <li><a href="https://github.com/Borumer/Chatfish/releases/download/v0.1-alpha.2/publishv0.1-alpha.2.zip">Download for Windows</a></li>
+        <li>Use the Start menu or the File Explorer to find the EXE after extracting the ZIP file</li>
+    </ol>
+</div>
+<div>
+    <h2>Unix (Mac and Linux)</h2>
+    <p>Unfortunately, Chatfish does not have support for Windows</p>
+</div>
+</body>
+</html>
